d73b2560a28f64e4ce4519f9e31634b61d42edf2
Julio M Cruz <julio.cruz@eb-ms.net>
2024-12-17 20:34:17 -0500

    Update controls

--- MVP-App/app/(dashboard)/dashboard/page.tsx
+++ MVP-App/app/(dashboard)/dashboard/page.tsx
@@ -4,8 +4,16 @@ import { DashboardHeader } from '@/components/dashboard/header';
 import { DashboardStats } from '@/components/dashboard/stats';
 import { UpcomingMeetings } from '@/components/dashboard/upcoming-meetings';
 import { RecentActivity } from '@/components/dashboard/recent-activity';
+import { useDynamicContext, useIsLoggedIn } from '@dynamic-labs/sdk-react-core'
 
 export default function DashboardPage() {
+
+  const { user } = useDynamicContext();
+  const isLoggedIn = useIsLoggedIn();
+
+  console.log('user', user);
+  console.log('isLoggedIn', isLoggedIn);
+
   return (
     <div className="space-y-8">
       <DashboardHeader />
